499798a3449da3a01597965ca52925a86feef3bd
Simon Liu <sim.liu92@gmail.com>
2020-02-25 20:35:22 -0500

    Add recursive approach

--- 101.symmetric-tree.py
+++ 101.symmetric-tree.py
@@ -5,6 +5,30 @@
 #         self.left = None
 #         self.right = None
 
+ # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution2:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root and root.left and root.right:
+            return self.isMirror(root.left, root.right)
+        elif not root or (not root.left and not root.right):
+            return True
+        else:
+            return False
+    
+    def isMirror(self, t1, t2):
+        if (t1 is None and t2 is not None) or (t1 is not None and t2 is None):
+            return False
+        if t1 is None and t2 is None:
+            return True
+        
+        return (t1.val == t2.val) and self.isMirror(t1.left, t2.right) and self.isMirror(t1.right, t2.left)
+
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
         # iteratively
